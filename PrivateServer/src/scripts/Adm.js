29b77ec0c4debbc511ffd52c15eeecc24673ccd5
Vinicius-Batista10 <viniciusasdqwe7@gmai.com>
2026-05-03 03:45:42 -0300

    fix: Structure and remove unnecessary assets for admin

--- PrivateServer/src/scripts/Adm.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sidebar = document.getElementById("sidebar");
-    const toggleBtn = document.getElementById("toggleSidebar");
-
-    toggleBtn.addEventListener("click", (e) => {
-        e.stopPropagation(); 
-        sidebar.classList.toggle("open");
-    });
-});
--- PrivateServer/src/scripts/employee.js
+++ PrivateServer/src/scripts/employee.js
@@ -41,3 +41,15 @@ document.addEventListener("DOMContentLoaded", () => {
         sidebar.classList.toggle("open");
     });
 });
+function sairFuncionario() {
+    sessionStorage.clear();
+    window.location.href = 'login.html';
+}
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('sidebar-overlay');
+    
+    sidebar.classList.toggle('ativo');
+    overlay.classList.toggle('ativo');
+}
